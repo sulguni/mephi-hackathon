0341227b3c28b20ef8d5e92dce1ae998224e17fc
Tema <happylife2769@gmail.com>
2025-07-18 17:20:39 +0300

    start and register phone

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
+import states
 
 # Bot token can be obtained via https://t.me/BotFather
 TOKEN = getenv("BOT_TOKEN")
@@ -18,24 +20,15 @@ dp = Dispatcher()
 
 
 @dp.message(CommandStart())
-async def command_start_handler(message: Message) -> None:
-    pass
+async def echo_handler(message: Message, state: FSMContext) -> None:
 
+    await message.answer('Привет, для начала тебе нужно зарегестрироваться.\n'
+                         ' Пожалуйста, введи свой номер телефона')
+    await state.set_state(states.UserState.phone)
 
-@dp.message()
-async def echo_handler(message: Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
-
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
-    """
-    try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
-
+@dp.message(states.UserState.phone)
+async def admin_newsletter_step_2(message: Message):
+    print(message.text)
 
 async def main() -> None:
     # Initialize Bot instance with default bot properties which will be passed to all API calls
